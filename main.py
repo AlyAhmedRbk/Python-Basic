2d2eae183834784870cf35d31f50fb27d987f17f
Aly Ahmed <113694375+AlyAhmedRbk@users.noreply.github.com>
2022-09-16 18:19:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hellow I Am Ali Ahmed ")
+print ("123")
+print (2*2)
+print (2**23)
+name = "ALi Ahmed"
+print (name + "  Hellow There")
+a= 5
+b=10
+print(a,b)
+print(a+b)
+
+sum = a+a+a+10.5
+
+print (sum)
+
+print (sum * sum)
+
+myString = "Hellow World"
+
+print(myString)
